fea75a129b90f972abda2a75933c78067838fcc9
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-09-16 06:00:47 +0530

    chore: first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: ['url-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    })
+  ],
+  devServer: {
+    static: './dist',
+    host: 'localhost',
+    port: port,
+    historyApiFallback: true,
+  }
+};
